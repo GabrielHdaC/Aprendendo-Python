43bde68f338374bcbe7c78b8a730200b4bd6e654
GabrielHdaC <gabrielhcunha2006@gmail.com>
2024-05-13 22:02:57 -0300

    Primeiro código em Python

---
+++ main.py
@@ -0,0 +1,20 @@
+# Primeiro Código Leigo em Python
+
+# Aprendendo sobre Input:
+
+# Unico problema do input é que ele retorna 
+# os valores como string, é preciso fazer a conversão 
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: ")) 
+peso = float(input("Digite seu peso: "))
+
+# Uma forma para saber o tipo da variável é usando o Type depois do print
+# Ele vai retornar o tipo da variável
+ 
+# Colocando o type ao entorno das variáveis idade e peso, para saber se está retornando corretamente.
+print(nome)
+print(idade)
+print(peso)
+print("---------------")
+print(type(idade))
+print(type(peso))
